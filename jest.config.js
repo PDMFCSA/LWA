f2ced16d4a137d45e1ecd9a07155b35a4a092ae6
DiogoCardoso <diogo.cardoso@pdmfc.com>
2025-03-18 16:05:00 +0000

    LWA-247- test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,19 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|mjs|cjs)$': 'babel-jest',
   },
+  collectCoverageFrom: [
+    '**/src/**/*.[jt]s',
+    '!**/src/**/*.spec.[jt]s',
+    '!**/src/**/*.mock.[jt]s',
+    '!**/src/**/*.e2e.[jt]s',
+    '!**/tests/**/*.test.[jt]s',
+],
+coverageDirectory: "./workdocs/coverage",
+coverageReporters: ["text", "lcov", "json-summary"],
+reporters: [
+  "default",
+  ["jest-junit", {outputDirectory: './workdocs/coverage', outputName: "junit-report.xml"}]
+]
+
+
 };
